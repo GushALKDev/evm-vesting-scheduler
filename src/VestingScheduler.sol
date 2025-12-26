3cb8bd01e20bcb95b772d06cdb2a9ef98febbc65
GushALKDev <gustavomartindeveloper@gmail.com>
2025-12-26 12:59:10 +0100

    Replace ERC20 Interface for OZ IERC20

--- src/VestingScheduler.sol
+++ src/VestingScheduler.sol
@@ -1,15 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-/**
- * @title IERC20 Interface
- * @notice Minimal ERC20 interface for token transfers
- */
-interface IERC20 {
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title VestingScheduler
